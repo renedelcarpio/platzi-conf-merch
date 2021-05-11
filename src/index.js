be2c92237423ebae2ba2a37e1dc544eeae76258f
Rene Del Carpio <delcarpio.rene@gmail.com>
2021-05-11 10:34:15 -0400

    se configuró webpack y se corrigió el error en ReactDom de index.js por el cual no retornaba nada en le navegador

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom'
 
 import App from './components/App'
 
-ReactDom.render(<App/>, document.getElementById('app'))
+ReactDOM.render(<App/>, document.getElementById('app'))
