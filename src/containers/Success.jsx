344af995c1c16245cbea0738c2ab2c106c5b129f
Rene Del Carpio <delcarpio.rene@gmail.com>
2021-05-18 11:41:09 -0400

    Se instaló @react-google-maps/api y se le dió funcionalidad a Success

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import '../styles/components/Success.css';
+import AppContext from '../context/AppContext'
+import Map from '../components/Map'
 
 const Success = () => {
+  const { state } = useContext(AppContext)
+  const { buyer } = state
   return (
     <div className="success">
       <div className="success-content">
-        <h2>Nombre, Gracias por tu compra!</h2>
+        <h2>{`${buyer.name}, Gracias por tu compra!`}</h2>
         <span>Tu pedido llegará en 3 días a tu dirección</span>
-        <div className="success-map">Google Maps</div>
+        <div className="success-map">
+          <Map/>
+        </div>
       </div>
     </div>
   );
