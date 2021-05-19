90e6269354d15c8bd4889e3eb8d61198bb6c7f3c
Rene Del Carpio <delcarpio.rene@gmail.com>
2021-05-19 10:28:52 -0400

    se creo el hook useGoogleAddress y se dio funcionalidad a Success y map, también se instaló axios

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-const Map = () => {
+const API_KEY = 'AIzaSyAIbEziUAOwAHRY5ZmsTVv0lUfan104C2g'
+
+const Map = ({data}) => {
   const mapStyles = {
     height: '50vh',
     width: '100%',
   };
 
   const defaultCenter = {
-    lat: 19.4267261,
-    lng: -99.1718796,
+    lat: data.lat,
+    lng: data.lng,
   };
   return (
-    <LoadScript googleMapsApiKey="AIzaSyAIbEziUAOwAHRY5ZmsTVv0lUfan104C2g">
+    <LoadScript googleMapsApiKey={API_KEY}>
       <GoogleMap mapContainerStyle={mapStyles} zoom={9} center={defaultCenter}>
         <Marker position={defaultCenter} />
       </GoogleMap>
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -3,17 +3,19 @@ import React, { useContext } from 'react';
 import '../styles/components/Success.css';
 import AppContext from '../context/AppContext';
 import Map from '../components/Map';
+import useGoogleAddress from '../hooks/useGoogleAddress'
 
 const Success = () => {
   const { state } = useContext(AppContext);
   const { buyer } = state;
+  const location = useGoogleAddress(buyer[0].address)
   return (
     <div className="success">
       <div className="success-content">
         <h2>{`${buyer.name}, Gracias por tu compra!`}</h2>
         <span>Tu pedido llegará en 3 días a tu dirección</span>
         <div className="success-map">
-          <Map />
+          <Map data={location}/>
         </div>
       </div>
     </div>
